e9dbfa6ada49260b6fd0f59c148da6fab6903a21
Ahammad204 <Ahammadcode@gmail.com>
2023-09-19 23:11:44 +0600

    fetch users data

--- src/main.jsx
+++ src/main.jsx
@@ -51,8 +51,8 @@ const router =  createBrowserRouter([
     {
 
       path:'/users',
+      loader:()=> fetch('https://jsonplaceholder.typicode.com/users'),
       element:<Users></Users>
-
 
     }
 
